76b7a54b8eb8447e1740c0473cdc02710d453942
翔吾 菅野 <ksyougo0918@gmail.com>
2020-03-13 15:41:44 +0900

    追加

---
+++ TextureConverter/EncodeWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+
+using System.Windows.Forms;
+
+namespace TextureConverter
+{
+    public class PrefList
+    {
+        public ObservableCollection<string> Data { get; }
+
+        public PrefList()
+        {
+            Data = new ObservableCollection<string>();
+            Data.Add("BC1");
+            Data.Add("BC1 SRGB");
+            Data.Add("BC2");
+            Data.Add("BC2 SRGB");
+            Data.Add("BC3");
+            Data.Add("BC3 SRGB");
+            Data.Add("BC4");
+            Data.Add("BC4 SRGB");
+            Data.Add("BC5");
+            Data.Add("BC5 SRGB");
+            Data.Add("BC6H");
+            Data.Add("BC6H SRGB");
+            Data.Add("BC7");
+            Data.Add("BC7 SRGB");
+        }
+    }
+
+    /// <summary>
+    /// EncodeWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class EncodeWindow : Window
+    {
+        private string InputFile = "";          // 使用する画像ファイル
+        private string FileName = "";           // 出力ファイル名
+        private string Folder = "";             // 出力先
+        private string OutputType = "";         // BCの種類
+        private string EncodeType = "";         // エンコードの方法
+
+
+        public EncodeWindow(string file)
+        {
+            InitializeComponent();
+
+            InputFile = file;
+
+            ComboBox.Items.Add("BC1_UNORM");
+            ComboBox.Items.Add("BC1_UNORM_SRGB");
+            ComboBox.Items.Add("BC2_UNORM");
+            ComboBox.Items.Add("BC2_UNORM_SRGB");
+            ComboBox.Items.Add("BC3_UNORM");
+            ComboBox.Items.Add("BC3_UNORM_SRGB");
+            ComboBox.Items.Add("BC4_UNORM");
+            ComboBox.Items.Add("BC4_UNORM_SRGB");
+            ComboBox.Items.Add("BC5_UNORM");
+            ComboBox.Items.Add("BC5_UNORM_SRGB");
+            ComboBox.Items.Add("BC6H_UF16");
+            ComboBox.Items.Add("BC6H_SF16");
+            ComboBox.Items.Add("BC7_UNORM");
+            ComboBox.Items.Add("BC7_UNORM_SRGB");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = (System.Windows.Controls.RadioButton)sender;
+
+            // エンコード方法の設定
+            if("GPU 使用" == radioButton.Content.ToString())
+            {
+                EncodeType = "gpu";
+            }
+            else
+            {
+                EncodeType = "nogpu";
+            }
+        }
+
+        private void FD(object sender, RoutedEventArgs e)
+        {
+            FolderDialog();
+        }
+
+        private void Run(object sender, RoutedEventArgs e)
+        {
+            var proc = new System.Diagnostics.Process();
+
+            proc.StartInfo.FileName = "date\\texconv.exe";
+
+            proc.StartInfo.Arguments = "-f " + OutputType + " -srgb " + EncodeType + " -o " + Folder + " -y " + InputFile;
+
+            proc.Start();
+
+            proc.WaitForExit();
+
+            System.Windows.MessageBox.Show("終了しました");
+
+            this.Close();
+        }
+
+        // <summary>
+        // フォルダダイアログボックスを表示
+        // </summary>
+        private void FolderDialog()
+        {
+            // ダイアログのインスタンスを生成
+            var browser = new System.Windows.Forms.FolderBrowserDialog();
+
+            browser.Description = "フォルダーを選択してください";
+
+            // ダイアログを表示する
+            if (browser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.Folder = browser.SelectedPath;
+
+                this.OutputFolder.Text = Folder;
+
+                var i = 0;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OutputType = ComboBox.SelectedValue.ToString();
+        }
+    }
+}
--- TextureConverter/MainWindow.xaml.cs
+++ TextureConverter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,169 @@ namespace TextureConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BitmapImage bitmap = null;
+
+        private Point _start;
+
+        public string FileName = "";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            bitmap = new BitmapImage(); // デコードされたビットマップイメージのインスタンスを作る。
+        }
+
+        private void event_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy; // ドラッグ中のカーソルを変える。
+        }
+
+        private void event_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) // ドロップされたものがファイルかどうか確認する。
+            {
+                FileName = ((string[])e.Data.GetData(DataFormats.FileDrop))[0]; // ドロップされた最初のファイルのファイル名を得る。
+
+                if(null != bitmap)
+                {
+                    bitmap = null;
+                    bitmap = new BitmapImage(); // デコードされたビットマップイメージのインスタンスを作る。
+                }
+
+                LoadTexture(bitmap, FileName);
+            }
+        }
+
+        private void FileDialog()
+        {
+            // ダイアログのインスタンスを生成
+            var dialog = new OpenFileDialog();
+
+            // ファイルの種類を設定
+            dialog.Filter = "画像ファイル (*.png)|*.png|(*.jpg)|*.jpg|(*.bmp)|*.bmp|(*.gif)|*.gif|(*.tiff)|*.tiff|全てのファイル (*.*)|*.*";
+
+            // ダイアログを表示する
+            if (dialog.ShowDialog() == true)
+            {
+                if (null != bitmap)
+                {
+                    bitmap = null;
+                    bitmap = new BitmapImage(); // デコードされたビットマップイメージのインスタンスを作る。
+                }
+
+                FileName = dialog.FileName;
+
+                LoadTexture(bitmap, FileName);
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuitem = (MenuItem)sender; // オブジェクトをMenuItemクラスのインスタンスにキャストする。
+            string header = menuitem.Header.ToString(); // Headerプロパティを取り出して、文字列に変換する。
+            string tag = menuitem.Tag.ToString(); // Tagプロパティを取り出して、文字列に変換する。
+
+            switch (tag) // Tag文字列毎の処理
+            {
+                case "file":
+                    FileDialog();
+                    break;
+
+                case "file_close":
+                    image.Source = null;
+                    break;
+
+                case "encode":
+                    Open_EncodeWindow();
+                    break;
+
+                case "close":
+                    this.Close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        private void Image_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            // スケールの値を変えることでホイールを動かした時の拡大率を制御できます
+            const float scale = 1.2f;
+
+            var matrix = image.RenderTransform.Value;
+            if (e.Delta > 0)
+            {
+                // 拡大処理
+                matrix.ScaleAt(scale, scale, e.GetPosition(this).X, e.GetPosition(this).Y);
+            }
+            else
+            {
+                // 縮小処理
+                matrix.ScaleAt(1.0f / scale, 1.0f / scale, e.GetPosition(this).X, e.GetPosition(this).Y);
+            }
+
+            image.RenderTransform = new MatrixTransform(matrix);
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            image.CaptureMouse();
+            _start = e.GetPosition(Border1);
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (image.IsMouseCaptured)
+            {
+                var matrix = image.RenderTransform.Value;
+
+                Vector v = _start - e.GetPosition(Border1);
+                matrix.Translate(-v.X, -v.Y);
+                image.RenderTransform = new MatrixTransform(matrix);
+                _start = e.GetPosition(Border1);
+            }
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            image.ReleaseMouseCapture();
+        }
+
+
+
+        private void LoadTexture(BitmapImage bitmap, string filename)
+        {
+            try
+            {
+                bitmap.BeginInit();
+
+                if (null != bitmap.UriSource) bitmap.UriSource = null;
+                bitmap.UriSource = new Uri(filename); // ビットマップイメージのソースにファイルを指定する。
+
+                bitmap.EndInit();
+                image.Source = bitmap; // Imageコントロールにバインディングする。
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Open_EncodeWindow()
+        {
+            if(null != bitmap.UriSource)
+            {
+                EncodeWindow encode_window = new EncodeWindow(FileName);
+                encode_window.Owner = this;
+                encode_window.Show();
+            }
+            else
+            {
+                MessageBox.Show("画像ファイルが読み込まれていません");
+            }
         }
     }
 }
